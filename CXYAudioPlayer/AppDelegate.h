d53c826c057e3e18068eee1f696063066c34188f
JCCP3 <616613995@qq.com>
2016-01-07 10:51:14 +0800

    创建master分之 提交初始代码

---
+++ CXYAudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CXYAudioPlayer
+//
+//  Created by JC_CP3 on 16/1/7.
+//  Copyright © 2016年 JC_CP3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CXYAudioPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CXYAudioPlayer
+//
+//  Created by JC_CP3 on 16/1/7.
+//  Copyright © 2016年 JC_CP3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
